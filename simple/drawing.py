a9d0e0f88f2d3825c0ef9e91ec02dbced988b3d5
Tony Tong <ttong@pro-ai.org>
2020-03-20 21:59:40 -0700

    Add more functions

--- simple/drawing.py
+++ simple/drawing.py
@@ -1,6 +1,67 @@
 from turtle import *
 
 
+def switchpen():
+    if isdown():
+        pu()
+    else:
+        pd()
+
+
+def demo1():
+    """Demo of old turtle.py - module"""
+    reset()
+    tracer(True)
+    up()
+    backward(100)
+    down()
+    # draw 3 squares; the last filled
+    width(3)
+    for i in range(3):
+        if i == 2:
+            begin_fill()
+        for _ in range(4):
+            forward(20)
+            left(90)
+        if i == 2:
+            color("maroon")
+            end_fill()
+        up()
+        forward(30)
+        down()
+    width(1)
+    color("black")
+    # move out of the way
+    tracer(False)
+    up()
+    right(90)
+    forward(100)
+    right(90)
+    forward(100)
+    right(180)
+    down()
+    # some text
+    write("startstart", 1)
+    write("start", 1)
+    color("red")
+    # staircase
+    for i in range(5):
+        forward(20)
+        left(90)
+        forward(20)
+        right(90)
+    # filled staircase
+    tracer(True)
+    begin_fill()
+    for i in range(5):
+        forward(20)
+        left(90)
+        forward(20)
+        right(90)
+    end_fill()
+    # more text
+
+
 def demo2():
     """Demo of some new features."""
     speed(1)
@@ -101,9 +162,9 @@ def demo2():
     tri.onclick(baba, 1)
 
 
+demo1()
 demo2()
 exitonclick()
-
 
 # turtle.bgcolor("yellow")
 # turtle.home()
